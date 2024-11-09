fc83459bdafcee98908f3a0767072dc8b1812982
Finn Panhuijsen <FB.Panhuijsen@student.han.nl>
2024-11-09 16:39:53 +0100

    Added validation to loginrequests

--- backend/src/main/java/han/se/fswd/tep/module/LoginRequest.java
+++ backend/src/main/java/han/se/fswd/tep/module/LoginRequest.java
@@ -5,12 +5,14 @@ public class LoginRequest {
     private String username;
     private String password;
 
-
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
+        if (username == null || username.length() < 3 || username.length() > 16) {
+            throw new IllegalArgumentException("Username must be between 3 and 16 characters.");
+        }
         this.username = username;
     }
 
@@ -19,6 +21,10 @@ public class LoginRequest {
     }
 
     public void setPassword(String password) {
+        // Max of 60 is based on the database max length, not the case description itself.
+        if (password == null || password.length() < 3 || password.length() > 60) {
+            throw new IllegalArgumentException("Password must be between 3 and 60 characters.");
+        }
         this.password = password;
     }
 }
